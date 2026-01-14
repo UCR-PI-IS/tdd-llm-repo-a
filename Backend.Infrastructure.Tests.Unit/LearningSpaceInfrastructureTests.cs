3d85d8fbc435836498c69f8715081a3f6bbe61bc
Jose Pablo Ramírez Méndez <89355471+pablodevrzmz@users.noreply.github.com>
2026-01-13 19:51:57 -0600

    Add Infrastructure layer tests for listing learning components user story

---
+++ Backend.Infrastructure.Tests.Unit/LearningSpaceInfrastructureTests.cs
@@ -0,0 +1,46 @@
+using NUnit.Framework;
+using System;
+using UCR.ECCI.PI.ThemePark.Backend.Infrastructure.Repositories;
+using UCR.ECCI.PI.ThemePark.Backend.Domain.Entities;
+using UCR.ECCI.PI.ThemePark.Backend.Domain.Exceptions;
+using System.Collections.Generic;
+
+namespace UCR.ECCI.PI.ThemePark.Backend.Infrastructure.Tests.Unit
+{
+    [TestFixture]
+    public class LearningSpaceInfrastructureTests
+    {
+        private SqlLearningSpaceListRepository repository;
+
+        [SetUp]
+        public void Setup()
+        {
+            repository = new SqlLearningSpaceListRepository();
+        }
+
+        [Test(Description = "Returns list of components for valid learning space ID")]
+        public void GetComponentsByLearningSpaceId_ValidId_ReturnsComponents()
+        {
+            var components = repository.GetComponentsByLearningSpaceId(1);
+            Assert.IsNotNull(components);
+            Assert.IsTrue(components.Count > 0);
+        }
+
+        [Test(Description = "Returns empty list for valid learning space ID with no components")]
+        public void GetComponentsByLearningSpaceId_ValidIdNoComponents_ReturnsEmptyList()
+        {
+            var components = repository.GetComponentsByLearningSpaceId(2);
+            Assert.IsNotNull(components);
+            Assert.AreEqual(0, components.Count);
+        }
+
+        [Test(Description = "Throws exception for invalid learning space ID")]
+        public void GetComponentsByLearningSpaceId_InvalidId_ThrowsInvalidLearningSpaceException()
+        {
+            Assert.Throws<InvalidLearningSpaceException>(() =>
+            {
+                repository.GetComponentsByLearningSpaceId(-1);
+            });
+        }
+    }
+}
