ee8d21e4539100c8bc9ce1e902848cfe7fcd5f09
Jose Pablo Ramírez Méndez <89355471+pablodevrzmz@users.noreply.github.com>
2026-01-15 21:20:24 -0600

    Implement ListComponents and AddComponent in LearningSpace entity, include LearningComponent entity

--- Backend.Domain/Entities/LearningSpace.cs
+++ Backend.Domain/Entities/LearningSpace.cs
@@ -1,4 +1,7 @@
-﻿namespace UCR.ECCI.PI.ThemePark.Backend.Domain.Entities;
+namespace UCR.ECCI.PI.ThemePark.Backend.Domain.Entities;
+
+using System;
+using System.Collections.Generic;
 
 /// <summary>
 /// Represents a learning space in a building of the theme park UCR.
@@ -8,27 +11,29 @@ public class LearningSpace
     /// <summary>
     /// Unique identifier for the learning space.
     /// </summary>
-    public String id { get; }
+    public string Id { get; }
 
     /// <summary>
     /// Type of the learning space (e.g., classroom, lab and auditorium).
     /// </summary>
-    public String type { get; }
+    public string Type { get; }
 
     /// <summary>
     /// Height of the learning space in meters.
     /// </summary>
-    public float height { get; }
+    public float Height { get; }
 
     /// <summary>
     /// Width of the learning space in meters.
     /// </summary>
-    public float width { get; }
+    public float Width { get; }
 
     /// <summary>
     /// Length of the learning space in meters.
     /// </summary>
-    public float length { get; }
+    public float Length { get; }
+
+    private readonly List<LearningComponent> _components = new();
 
     /// <summary>
     /// Constructor for the LearningSpace class.
@@ -38,12 +43,42 @@ public class LearningSpace
     /// <param name="height">Height of the learning space in meters</param>
     /// <param name="width">Width of the learning space in meters</param>
     /// <param name="length">Length of the learning space in meters</param>
-    public LearningSpace(String id, String type, float height, float width, float length)
+    public LearningSpace(string id, string type, float height, float width, float length)
     {
-        this.id = id;
-        this.type = type;
-        this.height = height;
-        this.width = width;
-        this.length = length;
+        Id = id;
+        Type = type;
+        Height = height;
+        Width = width;
+        Length = length;
+    }
+
+    /// <summary>
+    /// Adds a component to the learning space.
+    /// </summary>
+    /// <param name="component">Component to add</param>
+    public void AddComponent(LearningComponent component)
+    {
+        if (component == null) throw new ArgumentNullException(nameof(component));
+        _components.Add(component);
+    }
+
+    /// <summary>
+    /// Lists all components in the learning space.
+    /// </summary>
+    /// <returns>List of components</returns>
+    public IReadOnlyList<LearningComponent> ListComponents()
+    {
+        return _components.AsReadOnly();
+    }
+}
+
+// Class for LearningComponent entity (if it does not exist yet)
+public class LearningComponent
+{
+    public string Name { get; }
+
+    public LearningComponent(string name)
+    {
+        Name = name ?? throw new ArgumentNullException(nameof(name));
     }
 }
