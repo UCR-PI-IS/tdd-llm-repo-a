36edc6ec146d2be1394ad27699f604fd35de895c
Jose Pablo Ramírez Méndez <89355471+pablodevrzmz@users.noreply.github.com>
2026-01-15 21:28:51 -0600

    Add LearningComponentDto in Presentation layer Dtos

---
+++ Backend.Presentation/Dtos/LearningComponentDto.cs
@@ -0,0 +1,12 @@
+namespace UCR.ECCI.PI.ThemePark.Backend.Presentation.Dtos
+{
+    public class LearningComponentDto
+    {
+        public string Name { get; set; }
+
+        public LearningComponentDto(string name)
+        {
+            Name = name;
+        }
+    }
+}
