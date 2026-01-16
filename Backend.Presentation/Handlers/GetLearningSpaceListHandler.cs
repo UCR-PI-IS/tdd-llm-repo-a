a9e960d3c4136cd57c9ead2ab1ae598492a74cb6
Jose Pablo Ramírez Méndez <89355471+pablodevrzmz@users.noreply.github.com>
2026-01-15 21:29:48 -0600

    Fix using directives and namespace references in GetLearningSpaceListHandler

--- Backend.Presentation/Handlers/GetLearningSpaceListHandler.cs
+++ Backend.Presentation/Handlers/GetLearningSpaceListHandler.cs
@@ -4,23 +4,14 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 using UCR.ECCI.PI.ThemePark.Backend.Application.Services;
-using UCR.ECCI.PI.ThemePark.Backend.Presentation.Api.Dtos;
-using UCR.ECCI.PI.ThemePark.Backend.Presentation.Api.Responses;
+using UCR.ECCI.PI.ThemePark.Backend.Presentation.Dtos;
+using UCR.ECCI.PI.ThemePark.Backend.Presentation.Responses;
 using UCR.ECCI.PI.ThemePark.Backend.Domain.Exceptions;
 
 namespace UCR.ECCI.PI.ThemePark.Backend.Presentation.Api.Handlers
 {
-    /// <summary>
-    /// Handler for fetching learning components for a learning space.
-    /// </summary>
     public static class GetLearningSpaceListHandler
     {
-        /// <summary>
-        /// Handles the asynchronous GET request to retrieve components for a learning space by ID.
-        /// </summary>
-        /// <param name="learningSpaceId">The ID of the learning space.</param>
-        /// <param name="learningSpaceList">Service to retrieve learning space components.</param>
-        /// <returns>An HTTP result with either the list of components or an error response.</returns>
         public static async Task<Results<Ok<List<LearningComponentDto>>, NotFound<ErrorResponse>>> HandleAsync(
             [FromRoute] int learningSpaceId,
             [FromServices] ILearningSpaceListService learningSpaceList)
