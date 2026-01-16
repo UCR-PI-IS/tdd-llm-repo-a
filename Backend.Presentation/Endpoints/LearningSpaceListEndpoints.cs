5a2a248e09f83f5de57c5d1c474f13a9a3e06950
Jose Pablo Ramírez Méndez <89355471+pablodevrzmz@users.noreply.github.com>
2026-01-15 21:22:18 -0600

    Add GET endpoint for listing learning space components by ID

--- Backend.Presentation/Endpoints/LearningSpaceListEndpoints.cs
+++ Backend.Presentation/Endpoints/LearningSpaceListEndpoints.cs
@@ -1,23 +1,23 @@
-﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Routing;
 using UCR.ECCI.PI.ThemePark.Backend.Presentation.Api.Handlers;
 
 namespace UCR.ECCI.PI.ThemePark.Backend.Presentation.Api.Endpoints
 {
     /// <summary>
-    /// Contains endpoint mappings for the learning space list API.
+    /// Contains endpoint mappings for the learning space API.
     /// </summary>
     public static class LearningSpaceListEndpoints
     {
         /// <summary>
-        /// Maps the GET endpoint for fetching the list of learning spaces.
+        /// Maps the GET endpoint for fetching the list of learning components.
         /// </summary>
         /// <param name="builder">The <see cref="IEndpointRouteBuilder"/> used to map the endpoint.</param>
         /// <returns>The updated <see cref="IEndpointRouteBuilder"/> with the new route.</returns>
         public static IEndpointRouteBuilder MapLearningSpaceEndpoints(this IEndpointRouteBuilder builder)
         {
-            builder.MapGet("/LearningSpaceList", GetLearningSpaceListHandler.HandleAsync)
-                .WithName("GetLearningSpaceList")
+            builder.MapGet("/learning-spaces/{id}/components", GetLearningSpaceListHandler.HandleAsync)
+                .WithName("GetLearningSpaceComponents")
                 .WithOpenApi();
 
             return builder;
