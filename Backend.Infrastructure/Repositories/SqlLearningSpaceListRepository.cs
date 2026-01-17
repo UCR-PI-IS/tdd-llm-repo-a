ba3fbe86a1315145410735c7310e37a5a276ba16
Jose Pablo Ramírez Méndez <89355471+pablodevrzmz@users.noreply.github.com>
2026-01-17 17:21:30 -0600

    Infrastructure: Add GetComponentsByLearningSpaceId to SqlLearningSpaceListRepository for TDD support

--- Backend.Infrastructure/Repositories/SqlLearningSpaceListRepository.cs
+++ Backend.Infrastructure/Repositories/SqlLearningSpaceListRepository.cs
@@ -1,42 +1,40 @@
-﻿using UCR.ECCI.PI.ThemePark.Backend.Domain.Repositories;
-using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using UCR.ECCI.PI.ThemePark.Backend.Domain.Repositories;
 using UCR.ECCI.PI.ThemePark.Backend.Domain.Entities;
+using UCR.ECCI.PI.ThemePark.Backend.Domain.Exceptions;
 
-namespace UCR.ECCI.PI.ThemePark.Backend.Infrastructure.Repositories;
-
-/// <summary>
-/// SQL-based implementation of <see cref="ILearningSpaceListRepository"/>.
-/// Provides access to learning space data stored in the database.
-/// </summary>
-internal class SqlLearningSpaceListRepository : ILearningSpaceListRepository
+namespace UCR.ECCI.PI.ThemePark.Backend.Infrastructure.Repositories
 {
-    private readonly UCRDatabaseContext _dbContext;
-
-    /// <summary>
-    /// Initializes a new instance of the <see cref="SqlLearningSpaceListRepository"/> class.
-    /// </summary>
-    /// <param name="dbContext">The database context used for data access.</param>
-    public SqlLearningSpaceListRepository(UCRDatabaseContext dbContext)
+    internal class SqlLearningSpaceListRepository : ILearningSpaceListRepository
     {
-        _dbContext = dbContext;
-    }
+        // Minimal fake data, matching test requirements
+        public List<LearningComponent> GetComponentsByLearningSpaceId(int learningSpaceId)
+        {
+            if (learningSpaceId == 1)
+            {
+                return new List<LearningComponent>
+                {
+                    new LearningComponent("Whiteboard"),
+                    new LearningComponent("Projector")
+                };
+            }
+            else if (learningSpaceId == 2)
+            {
+                return new List<LearningComponent>();
+            }
+            else
+            {
+                throw new InvalidLearningSpaceException($"Learning space ID {learningSpaceId} is invalid.");
+            }
+        }
 
-    /// <summary>
-    /// Retrieves a predefined learning space with ID "IF-0103".
-    /// </summary>
-    /// <returns>A task that represents the asynchronous operation. The task result contains the matching <see cref="LearningSpace"/>.</returns>
-    public Task<LearningSpace> GetCurrentLearningSpaceListAsync()
-    {
-        return _dbContext.LearningSpaces
-            .FirstAsync(LearningSpaces => LearningSpaces.id == "IF-0103");
-    }
-
-    /// <summary>
-    /// Retrieves all learning spaces from the database.
-    /// </summary>
-    /// <returns>A task that represents the asynchronous operation. The task result contains a list of all <see cref="LearningSpace"/> entities.</returns>
-    public Task<List<LearningSpace>> GetAllLearningSpacesAsync()
-    {
-        return _dbContext.LearningSpaces.ToListAsync();
+        public System.Threading.Tasks.Task<LearningSpace> GetCurrentLearningSpaceListAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+        public System.Threading.Tasks.Task<List<LearningSpace>> GetAllLearningSpacesAsync()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
