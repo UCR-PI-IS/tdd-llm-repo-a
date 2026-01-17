77158613feeddc00a4494780acf3a32aeee14891
Jose Pablo Ramírez Méndez <89355471+pablodevrzmz@users.noreply.github.com>
2026-01-17 17:19:24 -0600

    Domain: Add InvalidLearningSpaceException for TDD failing test support

---
+++ Backend.Domain/Exceptions/InvalidLearningSpaceException.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace UCR.ECCI.PI.ThemePark.Backend.Domain.Exceptions
+{
+    public class InvalidLearningSpaceException : Exception
+    {
+        public InvalidLearningSpaceException() {}
+        public InvalidLearningSpaceException(string message) : base(message) {}
+    }
+}
