bbd8721eb385ea2bbd557e98d67f5a0f0a8e809b
Jose Pablo Ramírez Méndez <89355471+pablodevrzmz@users.noreply.github.com>
2026-01-17 21:20:28 -0600

    feat(presentation): add ErrorResponse DTO for error results on endpoints

---
+++ Backend.Presentation/Dtos/ErrorResponse.cs
@@ -0,0 +1,6 @@
+namespace UCR.ECCI.PI.ThemePark.Backend.Presentation.Dtos;
+
+/// <summary>
+/// Represents a simple error response DTO with one message.
+/// </summary>
+public record class ErrorResponse(string Message);
