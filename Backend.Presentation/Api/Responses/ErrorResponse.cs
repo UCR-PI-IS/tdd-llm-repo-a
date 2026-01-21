4d7f179961c6976f3a2a6a94f6a966bd9135934b
Jose Pablo Ramírez Méndez <89355471+pablodevrzmz@users.noreply.github.com>
2026-01-20 19:42:58 -0600

    Add ErrorResponse record in Backend.Presentation.Api.Responses

---
+++ Backend.Presentation/Api/Responses/ErrorResponse.cs
@@ -0,0 +1,6 @@
+namespace UCR.ECCI.PI.ThemePark.Backend.Presentation.Api.Responses;
+
+/// <summary>
+/// Represents an error response with a message.
+/// </summary>
+public record class ErrorResponse(string Message);
