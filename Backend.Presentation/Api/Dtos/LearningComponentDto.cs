fe28c76a2d03d807b3f7581846a892ea65b54a56
Jose Pablo Ramírez Méndez <89355471+pablodevrzmz@users.noreply.github.com>
2026-01-20 19:42:16 -0600

    Add LearningComponentDto record in Backend.Presentation.Api.Dtos

---
+++ Backend.Presentation/Api/Dtos/LearningComponentDto.cs
@@ -0,0 +1,7 @@
+namespace UCR.ECCI.PI.ThemePark.Backend.Presentation.Api.Dtos;
+
+/// <summary>
+/// Represents the data transfer object for a learning component.
+/// </summary>
+/// <param name="Name">The name of the learning component.</param>
+public record class LearningComponentDto(string Name);
