282f4bedb85dc6b52ab10c6f563044514747bb73
Jose Pablo Ramírez Méndez <89355471+pablodevrzmz@users.noreply.github.com>
2026-01-24 21:42:23 -0600

    Add ILearningComponentService interface to Application layer

--- Backend.Application/Services/ILearningComponentService.cs
+++ Backend.Application/Services/ILearningComponentService.cs
@@ -2,17 +2,7 @@ using UCR.ECCI.PI.ThemePark.Backend.Domain.Entities;
 
 namespace UCR.ECCI.PI.ThemePark.Backend.Application.Services;
 
-/// <summary>
-/// Service interface for managing learning component operations.
-/// </summary>
 public interface ILearningComponentService
 {
-    /// <summary>
-    /// Retrieves all learning components for a specific learning space.
-    /// </summary>
-    /// <param name="learningSpaceId">The identifier of the learning space.</param>
-    /// <returns>A collection of learning components.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when learningSpaceId is null.</exception>
-    /// <exception cref="ArgumentException">Thrown when learningSpaceId is empty.</exception>
     Task<IEnumerable<LearningComponent>> GetComponentsByLearningSpaceIdAsync(string learningSpaceId);
 }
