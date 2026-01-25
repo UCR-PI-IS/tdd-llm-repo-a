2a29e05be755625d138fe3071fddbffcdfc1c6af
Jose Pablo Ramírez Méndez <89355471+pablodevrzmz@users.noreply.github.com>
2026-01-24 22:17:52 -0600

    Domain: Add LearningComponent entity

--- Backend.Domain/Entities/LearningComponent.cs
+++ Backend.Domain/Entities/LearningComponent.cs
@@ -6,72 +6,52 @@ namespace UCR.ECCI.PI.ThemePark.Backend.Domain.Entities;
 public class LearningComponent
 {
     /// <summary>
-    /// Gets the unique identifier for the component.
+    /// Gets or sets the unique identifier for the learning component.
     /// </summary>
-    public int ComponentId { get; private set; }
+    public Guid ComponentId { get; set; }
 
     /// <summary>
-    /// Gets the identifier of the learning space this component belongs to.
+    /// Gets or sets the identifier of the associated learning space.
     /// </summary>
-    public string LearningSpaceId { get; private set; }
+    public string LearningSpaceId { get; set; } = string.Empty;
 
     /// <summary>
-    /// Gets the width of the component.
+    /// Gets or sets the width of the component.
     /// </summary>
-    public float Width { get; private set; }
+    public double Width { get; set; }
 
     /// <summary>
-    /// Gets the height of the component.
+    /// Gets or sets the height of the component.
     /// </summary>
-    public float Height { get; private set; }
+    public double Height { get; set; }
 
     /// <summary>
-    /// Gets the depth of the component.
+    /// Gets or sets the depth of the component.
     /// </summary>
-    public float Depth { get; private set; }
+    public double Depth { get; set; }
 
     /// <summary>
-    /// Gets the X coordinate position of the component.
+    /// Gets or sets the X coordinate position.
     /// </summary>
-    public float X { get; private set; }
+    public double X { get; set; }
 
     /// <summary>
-    /// Gets the Y coordinate position of the component.
+    /// Gets or sets the Y coordinate position.
     /// </summary>
-    public float Y { get; private set; }
+    public double Y { get; set; }
 
     /// <summary>
-    /// Gets the Z coordinate position of the component.
+    /// Gets or sets the Z coordinate position.
     /// </summary>
-    public float Z { get; private set; }
+    public double Z { get; set; }
 
     /// <summary>
-    /// Gets the orientation of the component.
+    /// Gets or sets the orientation of the component.
     /// </summary>
-    public string Orientation { get; private set; }
+    public double Orientation { get; set; }
 
     /// <summary>
-    /// Initializes a new instance of the <see cref="LearningComponent"/> class.
+    /// Gets or sets the navigation property to the associated learning space.
     /// </summary>
-    /// <param name="componentId">The unique identifier for the component.</param>
-    /// <param name="learningSpaceId">The identifier of the learning space.</param>
-    /// <param name="width">The width of the component.</param>
-    /// <param name="height">The height of the component.</param>
-    /// <param name="depth">The depth of the component.</param>
-    /// <param name="x">The X coordinate position.</param>
-    /// <param name="y">The Y coordinate position.</param>
-    /// <param name="z">The Z coordinate position.</param>
-    /// <param name="orientation">The orientation of the component.</param>
-    public LearningComponent(int componentId, string learningSpaceId, float width, float height, float depth, float x, float y, float z, string orientation)
-    {
-        ComponentId = componentId;
-        LearningSpaceId = learningSpaceId;
-        Width = width;
-        Height = height;
-        Depth = depth;
-        X = x;
-        Y = y;
-        Z = z;
-        Orientation = orientation;
-    }
+    public LearningSpace? LearningSpace { get; set; }
 }
