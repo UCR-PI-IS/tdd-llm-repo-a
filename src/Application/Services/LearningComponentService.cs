507735fe61e89e97230414119edd33648ebe2fbc
Jose Pablo Ramírez Méndez <89355471+pablodevrzmz@users.noreply.github.com>
2026-01-24 20:39:14 -0600

    Move LearningComponentService to Implementations namespace

--- src/Application/Services/LearningComponentService.cs
+++ src/Application/Services/LearningComponentService.cs
@@ -1,7 +1,7 @@
 using UCR.ECCI.PI.ThemePark.Backend.Application.Interfaces;
 using UCR.ECCI.PI.ThemePark.Backend.Domain.Entities;
 
-namespace UCR.ECCI.PI.ThemePark.Backend.Application.Services
+namespace UCR.ECCI.PI.ThemePark.Backend.Application.Services.Implementations
 {
     public class LearningComponentService : ILearningComponentService
     {
@@ -14,8 +14,11 @@ namespace UCR.ECCI.PI.ThemePark.Backend.Application.Services
 
         public async Task<IEnumerable<LearningComponent>> GetComponentsByLearningSpaceIdAsync(string learningSpaceId)
         {
+            if (learningSpaceId == null)
+                throw new ArgumentNullException(nameof(learningSpaceId));
+                
             if (string.IsNullOrWhiteSpace(learningSpaceId))
-                throw new ArgumentException("Learning space ID cannot be null or empty.", nameof(learningSpaceId));
+                throw new ArgumentException("Learning space ID cannot be empty.", nameof(learningSpaceId));
 
             return await _repository.GetByLearningSpaceIdAsync(learningSpaceId);
         }
