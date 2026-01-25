b7bc94e87f4ca916ec3cdcc6dc9b10242c118357
Jose Pablo Ramírez Méndez <89355471+pablodevrzmz@users.noreply.github.com>
2026-01-24 21:23:07 -0600

    Add ILearningComponentRepository interface to Domain layer

--- Backend.Domain/Repositories/ILearningComponentRepository.cs
+++ Backend.Domain/Repositories/ILearningComponentRepository.cs
@@ -3,14 +3,14 @@ using UCR.ECCI.PI.ThemePark.Backend.Domain.Entities;
 namespace UCR.ECCI.PI.ThemePark.Backend.Domain.Repositories;
 
 /// <summary>
-/// Repository interface for LearningComponent entity operations.
+/// Repository interface for managing LearningComponent entities.
 /// </summary>
 public interface ILearningComponentRepository
 {
     /// <summary>
     /// Retrieves all learning components associated with a specific learning space.
     /// </summary>
-    /// <param name="learningSpaceId">The identifier of the learning space</param>
-    /// <returns>A collection of learning components in the specified learning space</returns>
+    /// <param name="learningSpaceId">The identifier of the learning space.</param>
+    /// <returns>A collection of learning components belonging to the specified learning space.</returns>
     Task<IEnumerable<LearningComponent>> GetByLearningSpaceIdAsync(string learningSpaceId);
 }
