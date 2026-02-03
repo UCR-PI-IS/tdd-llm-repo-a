e5e17d25f5240254c50ea5029a8e9295b442fd6c
jpdevrzmz <jp.ramirez@bluecollar.cloud>
2026-02-02 19:31:51 -0600

    Presentation: Fix TypedResults namespace import

--- Backend.Presentation/Handlers/GetLearningComponentsHandler.cs
+++ Backend.Presentation/Handlers/GetLearningComponentsHandler.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.HttpResults;
 using UCR.ECCI.PI.ThemePark.Backend.Application.Services;
 using UCR.ECCI.PI.ThemePark.Backend.Presentation.Dtos;
